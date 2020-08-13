88189deb6420497400661306396a43bb6f313445
Eighty Three <cwp1@protonmail.com>
2020-08-13 16:45:34 +0800

    Add function for querying stations in database

---
+++ src/models/stations.ts
@@ -0,0 +1,31 @@
+import db from './db';
+const stationsTable = 'stations';
+const stationBranchTable = 'station_branch';
+const branchesTable = 'branches';
+
+export const checkStation = async (
+  stationName: string, 
+  argStationsTable: string = stationsTable, 
+  argStationBranchTable: string = stationBranchTable, 
+  argBranchesTable: string = branchesTable, 
+) => {
+  /*
+   * Returns user_id_followed if 
+   * followerUsername's user_id = user_id_follower AND
+   * followedUsername's user_id = user_id_followed
+   * * */
+  return await db.oneOrNone( 
+    'SELECT sb.branch_id, distance_to_branch_start, distance_to_branch_end, branch_start, branch_end from $2:alias s \
+    INNER JOIN $3:alias sb ON sb.station_id = s.station_id \
+    INNER JOIN $4:alias b ON sb.branch_id = b.branch_id WHERE station_name=$1 \
+    ', [stationName, argStationsTable, argStationBranchTable, argBranchesTable]
+  );
+};
+
+export const getStations = async (argStationsTable: string = stationsTable) => {
+  const arr = await db.many(
+    'SELECT (station_name) FROM $1:name', [argStationsTable]
+  );
+
+  return arr.map((station) => station.station_name);
+};
