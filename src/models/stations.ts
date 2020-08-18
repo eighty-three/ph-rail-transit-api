8bcd670ba46640aff697efb6e8515cd195b9ca55
Eighty Three <cwp1@protonmail.com>
2020-08-18 15:29:24 +0800

    Remove unnecessary params since unit tests were done on main database

--- src/models/stations.ts
+++ src/models/stations.ts
@@ -3,23 +3,18 @@ const stationsTable = 'stations';
 const stationBranchTable = 'station_branch';
 const branchesTable = 'branches';
 
-export const checkStation = async (
-  stationName: string, 
-  argStationsTable: string = stationsTable, 
-  argStationBranchTable: string = stationBranchTable, 
-  argBranchesTable: string = branchesTable, 
-) => {
+export const checkStation = async (stationName: string) => {
   return await db.oneOrNone( 
     'SELECT sb.branch_id, distance_to_branch_start, distance_to_branch_end, branch_start, branch_end from $2:alias s \
     INNER JOIN $3:alias sb ON sb.station_id = s.station_id \
     INNER JOIN $4:alias b ON sb.branch_id = b.branch_id WHERE station_name=$1 \
-    ', [stationName, argStationsTable, argStationBranchTable, argBranchesTable]
+    ', [stationName, stationsTable, stationBranchTable, branchesTable]
   );
 };
 
-export const getStations = async (argStationsTable: string = stationsTable) => {
+export const getStations = async () => {
   const arr = await db.many(
-    'SELECT (station_name) FROM $1:name', [argStationsTable]
+    'SELECT (station_name) FROM $1:name', [stationsTable]
   );
 
   return arr.map((station) => station.station_name);
