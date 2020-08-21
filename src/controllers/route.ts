81057daf9318052657028098a16287c31f282c64
Eighty Three <cwp1@protonmail.com>
2020-08-21 08:36:50 +0800

    Fix conditional to accomodate typing

--- src/controllers/route.ts
+++ src/controllers/route.ts
@@ -1,51 +1,46 @@
 import { RequestHandler } from 'express';
 import { shortestRoute, getMap, checkStation } from '../models';
-
-interface IGraph {
-  [key: string]: {
-    [key: string]: number
-  }
-}
+import { IGraph } from './route.types';
 
 const getRoute: RequestHandler = async (req, res) => {
   const start: string = req.query.start as string;
   const end: string = req.query.end as string;
-  const startStationBranch = await checkStation(start); // should return branch_id, distance_to_branch_start, distance_to_branch_end, branch_start, branch_end
-  const endStationBranch = await checkStation(end); // or returns nothing if intersection
+  const startStationInfo = await checkStation(start); // should return branch_id, distance_to_branch_start, distance_to_branch_end, branch_start, branch_end
+  const endStationInfo = await checkStation(end); // or returns nothing if intersection
   const map: IGraph = getMap();
 
-  if (startStationBranch && startStationBranch.branch_id === endStationBranch.branch_id) { // if queried stations are in the same branch
+  if (startStationInfo && endStationInfo && startStationInfo.branch_id === endStationInfo.branch_id) { // if queried stations are in the same branch
     /* If TAYUMAN to BAMBANG
      * TAYUMAN: {BAMBANG: someDistance}
      * BAMBANG: {TAYUMAN: someDistance}
      * delete map['DJOSE_RECTO]['ROOSEVELT']
      * delete map['ROOSEVELT']['DJOSE_RECTO']
      */ 
-    const distance = Math.abs(startStationBranch.distance_to_branch_start - endStationBranch.distance_to_branch_start);
+    const distance = Math.abs(startStationInfo.distance_to_branch_start - endStationInfo.distance_to_branch_start);
     map[start] = {[end]: distance};
     map[end] = {[start]: distance};
                                     
-    delete map[startStationBranch.branch_start][startStationBranch.branch_end];
-    delete map[startStationBranch.branch_end][startStationBranch.branch_start];
+    delete map[startStationInfo.branch_start][startStationInfo.branch_end];
+    delete map[startStationInfo.branch_end][startStationInfo.branch_start];
 
-    if (startStationBranch.distance_to_branch_start > endStationBranch.distance_to_branch_start) {
+    if (startStationInfo.distance_to_branch_start > endStationInfo.distance_to_branch_start) {
       // if queried stations are in the same branch, and `end` is closer to `branch_start`
-      map[end][endStationBranch.branch_start] = endStationBranch.distance_to_branch_start;
-      map[endStationBranch.branch_start][end] = endStationBranch.distance_to_branch_start;
+      map[end][endStationInfo.branch_start] = endStationInfo.distance_to_branch_start;
+      map[endStationInfo.branch_start][end] = endStationInfo.distance_to_branch_start;
 
-      map[start][startStationBranch.branch_end] = startStationBranch.distance_to_branch_end;
-      map[startStationBranch.branch_end][start] = startStationBranch.distance_to_branch_end;
+      map[start][startStationInfo.branch_end] = startStationInfo.distance_to_branch_end;
+      map[startStationInfo.branch_end][start] = startStationInfo.distance_to_branch_end;
 
     } else {
       // if queried stations are in the same branch, and `start` is closer to `branch_start`
-      map[start][startStationBranch.branch_start] = startStationBranch.distance_to_branch_start;
-      map[startStationBranch.branch_start][start] = startStationBranch.distance_to_branch_start;
+      map[start][startStationInfo.branch_start] = startStationInfo.distance_to_branch_start;
+      map[startStationInfo.branch_start][start] = startStationInfo.distance_to_branch_start;
 
-      map[endStationBranch.branch_end][end] = endStationBranch.distance_to_branch_end;
-      map[end][endStationBranch.branch_end] = endStationBranch.distance_to_branch_end;
+      map[endStationInfo.branch_end][end] = endStationInfo.distance_to_branch_end;
+      map[end][endStationInfo.branch_end] = endStationInfo.distance_to_branch_end;
     }
   } else { // else if queried stations are not in the same branch
-    if (startStationBranch) { // if not intersection, then fix map
+    if (startStationInfo) { // if not intersection, then fix map
       /* If TAYUMAN to MAGALLANES:
        * delete map['DJOSE_RECTO']['ROOSEVELT']
        * delete map['ROOSEVELT']['DJOSE_RECTO']
@@ -53,27 +48,27 @@ const getRoute: RequestHandler = async (req, res) => {
        * DJOSE_RECTO: {TAYUMAN: someDistance}
        * ROOSEVELT: {TAYUMAN: someDistance}
        */
-      delete map[startStationBranch.branch_start][startStationBranch.branch_end];
-      delete map[startStationBranch.branch_end][startStationBranch.branch_start];
+      delete map[startStationInfo.branch_start][startStationInfo.branch_end];
+      delete map[startStationInfo.branch_end][startStationInfo.branch_start];
       map[start] = {
-        [startStationBranch.branch_start]: startStationBranch.distance_to_branch_start,
-        [startStationBranch.branch_end]: startStationBranch.distance_to_branch_end
+        [startStationInfo.branch_start]: startStationInfo.distance_to_branch_start,
+        [startStationInfo.branch_end]: startStationInfo.distance_to_branch_end
       };
 
-      map[startStationBranch.branch_start][start] = startStationBranch.distance_to_branch_start;
-      map[startStationBranch.branch_end][start] = startStationBranch.distance_to_branch_end;
+      map[startStationInfo.branch_start][start] = startStationInfo.distance_to_branch_start;
+      map[startStationInfo.branch_end][start] = startStationInfo.distance_to_branch_end;
     }
 
-    if (endStationBranch) { // same as previous example but with MAGALLANES and its specific info this time
-      delete map[endStationBranch.branch_start][endStationBranch.branch_end];
-      delete map[endStationBranch.branch_end][endStationBranch.branch_start];
+    if (endStationInfo) { // same as previous example but with MAGALLANES and its specific info this time
+      delete map[endStationInfo.branch_start][endStationInfo.branch_end];
+      delete map[endStationInfo.branch_end][endStationInfo.branch_start];
       map[end] = {
-        [endStationBranch.branch_start]: endStationBranch.distance_to_branch_start,
-        [endStationBranch.branch_end]: endStationBranch.distance_to_branch_end
+        [endStationInfo.branch_start]: endStationInfo.distance_to_branch_start,
+        [endStationInfo.branch_end]: endStationInfo.distance_to_branch_end
       };
 
-      map[endStationBranch.branch_start][end] = endStationBranch.distance_to_branch_start;
-      map[endStationBranch.branch_end][end] = endStationBranch.distance_to_branch_end;
+      map[endStationInfo.branch_start][end] = endStationInfo.distance_to_branch_start;
+      map[endStationInfo.branch_end][end] = endStationInfo.distance_to_branch_end;
     }
   }
 
